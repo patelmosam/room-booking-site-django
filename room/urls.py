35705fdc2f47bc245f906ba3927e651332fb5008
patelmosam <patelmosam280@gmail.com>
2020-03-13 21:45:25 +0530

    changelog 6

--- room/urls.py
+++ room/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 	path('auth/', views.home, name="auth"),
 	path('costomer_home/', views.cust_home, name="c_home"),
 	path('book/', views.book_room, name="book_room"),
+	path('confirm_book/', views.confirm_book, name="confirm_book"),
 	path('view_book/', views.view_book, name='view_book'),
 	path('del_book/', views.render_delete, name="delete"),
 	path('delete/', views.delete, name='delete'),
@@ -20,7 +21,9 @@ urlpatterns = [
 	path('rmauth/', views.rm_auth, name="rmauth"),
 	path('rm_home/', views.rm_home, name="rm_home"),
 	path('room/', views.add_rooms, name='add_rooms'),
+	path('checkroom/', views.check_added_room, name='checkroom'),
 	path('del_room/', views.delete_room, name='del_room'),
 	path('del_auth/', views.delete_auth, name='del_auth'),
-	path('edit_room/', views.edit_rooms, name='edit_room')
+	path('edit_room/', views.edit_rooms, name='edit_room'),
+	
 ]
--- room/views.py
+++ room/views.py
@@ -91,6 +91,79 @@ def view_book(request):
 	return render(request, 'room/history.html', {'data': q})
 
 #done
+def confirm_book(request):
+	if request.method == "POST":
+		data = request.POST.copy()
+		date_opted = data.get("date_opted")
+		time_opted_in = data.get("time_opted_in")
+		tin = time_opted_in.split(':')
+		time_opted_out = data.get("time_opted_out")
+		tout = time_opted_out.split(':')
+		room_no = data.get("room_no")
+		today = str(datetime.date(datetime.now()))
+		bdate = date_opted.split('-')
+		tdate = today.split('-')
+		
+		for x in added_rooms.objects.all():
+			
+			if x.start_room <= int(room_no) and x.end_room >= int(room_no):
+				itime = x.in_time.split(':')
+				otime = x.out_time.split(':')
+				if int(tin[0]) < int(tout[0]):
+					print(itime[0],otime[0],tin[0])
+					if int(itime[0]) <= int(tin[0]) and int(otime[0]) >= int(tin[0]):
+						if int(otime[0]) >= int(tout[0]):
+							if int(tdate[2])+x.buffer_days <= int(bdate[2]):
+		 # checking in rooms_data	
+		 		
+								for d in rooms_data.objects.all():
+									idate = d.date_opted.split('-')
+									if int(idate[1]) == int(bdate[1]) and int(idate[2]) == int(bdate[2]) and int(idate[0]) == int(bdate[0]):
+										print(d.room_id,room_no)
+										if d.room_id == int(room_no):
+											it = d.time_in.split(':')
+											ot = d.time_out.split(':')
+											print(it,ot,tin)
+											if int(it[0]) <= int(tin[0]) and int(ot[0]) >= int(tin[0]):
+												return HttpResponse('room is already booked!!')
+											else:
+												if int(it[0]) <= int(tout[0]) and int(ot[0]) >= int(tout[0]):
+													return HttpResponse('room is already booked!!')
+									
+								context = add_booking(request, room_no, date_opted, time_opted_in, time_opted_out)
+								return render(request, "room/customer_home.html", context)
+			#   end checking in rooms_data
+				else:
+					return HttpResponse('please enter valid time slot')	
+			
+		return HttpResponse('room is not available for booking...')
+
+#done
+def add_booking(request, room_no, date_opted, time_opted_in, time_opted_out):
+	q = rooms_history()
+	q.action = "booking"
+	q.room_id = room_no
+	q.date_booking = datetime.date(datetime.now())
+	q.time_booking = datetime.time(datetime.now())
+	q.date_opted = str(date_opted)
+	q.time_opted = str('{} - {}'.format(time_opted_in, time_opted_out))
+	q.username = request.session["username"]
+	q.save()
+	q = rooms_data()
+	q.room_id = room_no
+	q.date_opted = str(date_opted)
+	q.time_in = str(time_opted_in)
+	q.time_out = str(time_opted_out)
+	q.is_booked = True
+	q.user_booked = request.session["username"]
+	q.save()
+	username = request.session["username"]
+	rdata = rooms_data.objects.all()
+	context = {'history':'/history/', 'book':'/book/', 'del_book':'/del_book/', 'view_book':'/cust_book/','username':username, 'data':rdata, 'table':True}
+	return context
+
+
+#done
 def render_delete(request):
 	q = [x for x in rooms_data.objects.all() if x.user_booked == request.session["username"]]
 	return render(request, 'room/delete.html', {'data': q})
@@ -184,6 +257,80 @@ def add_rooms(request):
 	else:
 		form = add_room()
 	return render(request, 'room/addrooms.html', {'form':form,'username':username})
+
+#done
+def check_added_room(request):
+	if request.method == "POST":
+		data = request.POST.copy()
+		try:
+			id = int(data.get('id'))
+			type_ = 'edit'
+		except:
+			id = None
+			type_ = 'new'
+		start_room = data.get("start_room")
+		end_room = data.get("end_room")
+		time_in = data.get("time_in")
+		tin = time_in.split(':')
+		time_out = data.get("time_out")
+		tout = time_out.split(':')
+		bdays = data.get("buffer_days")
+		if len(added_rooms.objects.all()) == 0:
+			context = add_slot(request, id, start_room, end_room, time_in, time_out, bdays, type_)
+			return render(request, 'room/manager_home.html', context)
+		else:
+			for x in added_rooms.objects.all():
+				if int(start_room) < int(end_room):
+					if x.start_room <= int(start_room) and x.end_room >= int(start_room):
+						if x.end_room >= int(end_room):
+							itime = x.in_time.split(':')
+							otime = x.out_time.split(':')
+							if int(tin[0]) < int(tout[0]):
+								if int(itime[0]) <= int(tin[0]) and int(otime[0]) >= int(tin[0]):
+									if int(otime[0]) >= int(tout[0]):
+										return HttpResponse('time slots are already added!')
+									else:
+										return HttpResponse('time slots is overlape with another one, please try again!!! ')
+							
+							else:
+								return HttpResponse('enter valid time slot')
+						else:
+							itime = x.in_time.split(':')
+							otime = x.out_time.split(':')
+							if int(tin[0]) < int(tout[0]):
+								if int(itime[0]) <= int(tin[0]) and int(otime[0]) >= int(tin[0]):
+									if int(otime[0]) >= int(tout[0]):
+										return HttpResponse('time slots are already added!')
+									else:
+										return HttpResponse('time slots is overlape with another one, please try again!!! ')
+				else:
+					return HttpResponse('enter valid rooms!')
+			context = add_slot(request, id, start_room, end_room, time_in, time_out, bdays, type_)
+			return render(request, 'room/manager_home.html', context)
+
+#done
+def add_rooms(request, id, start_room, end_room, time_in, time_out, bdays, type_):
+	username = request.session["rmname"]	
+	if type_ == 'new':
+		q = added_rooms()
+		q.start_room = start_room
+		q.end_room = end_room
+		q.in_time = str(time_in)
+		q.out_time = str(time_out)
+		q.buffer_days = bdays
+		q.save()
+	elif type_ == 'edit':
+		table = added_rooms.objects.get(id=id)
+		table.start_room = start_room
+		table.end_room = end_room
+		table.in_time = time_in
+		table.out_time = time_out
+		table.buffer_days = bdays
+		table.save()
+	data = added_rooms.objects.all()
+	context = {'room':'/room/', 'del_room':'/del_room/', 'edit_room':'/edit_room/', 'table': True, 'data': data, 'username':username}
+	return context
+
 
 #done
 def delete_room(request):
