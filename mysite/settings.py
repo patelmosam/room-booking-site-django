41720895628abefda17205f2edc8a956eed942e2
Gladmangee <r215774b@students.msu.ac.zw>
2023-04-25 11:03:04 +0200

    msu guest house
    guest house books

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'guestbookingdb',
+        'USER': 'postgres',
+        'PASSWORD': '26082001',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
