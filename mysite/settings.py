1b2e21e3618a6b4428d348565889a81b09975e84
GhostFulltech <developerselitemsu@gmail.com>
2023-04-26 16:58:04 +0200

    added few ui designes

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'guestbookingdb',
         'USER': 'postgres',
-        'PASSWORD': '26082001',
+        'PASSWORD': 'Fulltech#@4545',
         'HOST': 'localhost',
         'PORT': '5432',
     }
